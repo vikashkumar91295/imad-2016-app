374caeed848f788f27fd7c76dc5575cb917ac775
vikashkumar91295 <vikashkumar91295@gmail.com>
2016-10-10 02:00:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,18 @@
 console.log('Loaded!');
+
 // Change the text of the main-text div
-var element=document.getElementById('main-text');
-
-
+var element=document.getElementById('main-text'
+      );
+      
 element.innerHTML = 'New value';
 
 //move the image
-var image = document.getElementById('madi');
+var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight() {
     marginLeft=marginLeft +10;
-    image.style.marginLeft=marginLeft+ 'px';
+    image.style.marginLeft=marginLeft + 'px';
 }
 img.onclick = function(){
     var interval=setInterval(moveRight,100);
-    img.style.marginLeft= '100px';
 };
