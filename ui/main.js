6445d6b2df7c22fa27f0dddf26580ab6e81465ae
vikashkumar91295 <vikashkumar91295@gmail.com>
2016-10-10 01:35:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ var element=document.getElementById('main-text');
 
 
 element.innerHTML = 'New value';
+
+//move the image
+var image = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft= '100px';
+};
